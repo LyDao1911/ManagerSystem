c0ed080467a32cfaffab01fb07b9ec5d58293972
LyDao1911 <lydao19112004@gmail.com>
2025-04-13 17:55:43 +0700

    He thong quan ly Ca Heo QUan

---
+++ src/dao/Connect.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import java.sql.*;
+
+/**
+ *
+ * @author MINE
+ */
+public class Connect {
+    public static Connection getCon(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/chms","root","");
+            //chms là tên database
+            return con;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
---
+++ src/dao/DbOperations.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author MINE
+ */
+public class DbOperations {
+    public static void setDataOrDelete(String Query, String msg){
+        try{
+            Connection con = Connect.getCon();
+            Statement st = con.createStatement();
+            st.executeUpdate(Query);
+            if(!msg.equals(""))
+                JOptionPane.showMessageDialog(null, msg);
+    }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e, "Message", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
